d7ae964e757fe38399967cbcbb558ed3266e1de3
Kirill <parahinkv@gmail.com>
2022-07-06 22:10:21 +0300

    #14 - IEquatable <ICard>

--- CardGameDurak.Logic/Card.cs
+++ CardGameDurak.Logic/Card.cs
@@ -1,5 +1,6 @@
 ﻿using CardGameDurak.Abstractions;
 using CardGameDurak.Abstractions.Enums;
+using CardGameDurak.Abstractions.GameSession;
 using CardGameDurak.Abstractions.Players;
 
 namespace CardGameDurak.Logic;
@@ -7,7 +8,7 @@ namespace CardGameDurak.Logic;
 /// <summary xml:lang = "ru">
 /// Игральная карта.
 /// </summary>
-public class Card : ICard
+public class Card : IEquatable<ICard>
 {
     /// <summary xml:lang = "ru">
     /// Создает новый экземпляр класса <see cref="Card"/>.
@@ -32,4 +33,28 @@ public class Card : ICard
 
     /// <inheritdoc/>
     public IPlayer? Owner { get; set; }
+
+    /// <inheritdoc/>
+    public bool Equals(ICard? curCard)
+    {
+        if (curCard is null) 
+            return false;
+        if (Rank != curCard.Rank || Suit != curCard.Suit || Owner != curCard.Owner)
+            return false;
+        return true;
+    }
+
+    /// <inheritdoc/>
+    public override bool Equals(object? obj)
+    {
+        if (obj is null)
+            return false;
+        if (obj is ICard card)
+            return Equals(card);
+        else
+            return false;
+    }
+
+    /// <inheritdoc/>
+    public override int GetHashCode() => HashCode.Combine(Rank, Suit);
 }
